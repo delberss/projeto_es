75cb757ae42ce5bbef3baefd5e9c136f745b694d
delberss <delberss@hotmail.com>
2022-05-26 12:15:44 -0300

    Estilização de main, center,menu e login

--- src/App.js
+++ src/App.js
@@ -4,12 +4,17 @@ function App() {
   return (
     <div className="App">
       <div className='main'>
-        <div className='menu'>
-          <div className='logo'>
-            <h3>Projeto ES</h3>
-          </div>
-          <div className='item-menu'>
-              <a href='#'>Login</a>
+        <div className='center'>
+          <div className='menu'>
+            
+            <div className='logo'>
+              <h3>Projeto ES</h3>
+            </div>
+
+            <div className='item-menu'>
+                <a href='#'>Login</a>
+            </div>
+
           </div>
         </div>
       </div>
