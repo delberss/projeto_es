d35a63ce2ff28ee7750b43a782e205d7eb8e5748
delberss <delberss@hotmail.com>
2022-05-26 12:35:53 -0300

    Estilização da landing page

--- src/App.js
+++ src/App.js
@@ -17,7 +17,24 @@ function App() {
 
           </div>
         </div>
+
+
+        <div className='form'>
+        <h2>Entre em contato</h2>
+          <form>
+            <div className='items-form'>
+              <input placeholder='Seu nome...' type="text"></input>
+              <input placeholder='Seu email...' type="text"></input>
+              <input placeholder='Sua senha...' type="text"></input>
+              <input placeholder='Seu curso...' type="text"></input>
+              <input type="submit"></input>
+            </div>
+          </form>
+          
+        </div>
+
       </div>
+
     </div>
   );
 }
