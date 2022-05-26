7284fd5a4878a20bc7d1a3e6af0779af6ebbbe30
delberss <delberss@hotmail.com>
2022-05-26 12:03:41 -0300

    Apaga arqvs e cria componente main dentro do app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='main'>
+        <div className='menu'>
+          <div className='logo'>
+            <h3>Projeto ES</h3>
+          </div>
+          <div className='item-menu'>
+              <a href='#'>Login</a>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
